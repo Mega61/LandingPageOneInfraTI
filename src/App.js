2530c8761738c91f51a83f96d1e0c359bce3eae3
Juan E Daza <juandape@unisabana.edu.co>
2021-08-16 20:19:35 -0500

    Cambio declaracion app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import React from 'react'
 import BannerPrincipal from './BannerPrincipal';
 import BannerTwitch from './BannerTwitch';
-import img1 from './img/foto_twitch.png'
-import img2 from './img/fondo.png'
 
 function App() {
   return (
     <div className="App">
       <div id="LangingPage1Parcial1TI">
-        <img id="Fondo" src={img2} alt="FONDO"></img>
         <BannerPrincipal />
-        <img id="foto_twitch" src={img1} alt="ImagenStream"></img>
         <BannerTwitch />
       </div>
     </div>
--- src/BannerPrincipal.jsx
+++ src/BannerPrincipal.jsx
@@ -1,47 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import './BannerPrincipal.css'
 import img1 from './img/icono_kuma.png'
+import img2 from './img/fondo.png'
 
 export default class BannerPrincipal extends Component {
     render() {
         return (
-            <div id="complete_banner">
-                <svg class="banner_rectangle">
-                    <rect id="banner_rectangle" rx="0" ry="0" x="0" y="0" width="1920" height="110">
-                    </rect>
-                </svg>
-                <img id="icono_kuma" src={img1} alt="KumaICON"></img>
-                <div id="titulo_kuma">
-                    <span>KUMA FORCE</span>
-                </div>
-                <div id="home_div">
-                    <svg class="home_container">
-                        <rect id="home_container" rx="16" ry="16" x="0" y="-20" width="122" height="120">
+            <Fragment>
+                <img id="Fondo" src={img2} alt="FONDO"></img>
+                <div id="complete_banner">
+                    <svg class="banner_rectangle">
+                        <rect id="banner_rectangle" rx="0" ry="0" x="0" y="0" width="1920" height="110">
                         </rect>
                     </svg>
-                    <div id="Home">
-                        <span>Home</span>
+                    <img id="icono_kuma" src={img1} alt="KumaICON"></img>
+                    <div id="titulo_kuma">
+                        <span>KUMA FORCE</span>
+                    </div>
+                    <div id="home_div">
+                        <svg class="home_container">
+                            <rect id="home_container" rx="16" ry="16" x="0" y="-20" width="122" height="120">
+                            </rect>
+                        </svg>
+                        <div id="Home">
+                            <span>Home</span>
+                        </div>
+                    </div>
+                    <div id="about_div">
+                        <svg class="about_container">
+                            <rect id="about_container" rx="16" ry="16" x="0" y="-20" width="122" height="120">
+                            </rect>
+                        </svg>
+                        <div id="About">
+                            <span>About</span>
+                        </div>
+                    </div>
+                    <div id="contact_div">
+                        <svg class="contact_container">
+                            <rect id="contact_container" rx="16" ry="16" x="0" y="-20" width="122" height="120">
+                            </rect>
+                        </svg>
+                        <div id="Contact">
+                            <span>Contact</span>
+                        </div>
                     </div>
                 </div>
-                <div id="about_div">
-                    <svg class="about_container">
-                        <rect id="about_container" rx="16" ry="16" x="0" y="-20" width="122" height="120">
-                        </rect>
-                    </svg>
-                    <div id="About">
-                        <span>About</span>
-                    </div>
-                </div>
-                <div id="contact_div">
-                    <svg class="contact_container">
-                        <rect id="contact_container" rx="16" ry="16" x="0" y="-20" width="122" height="120">
-                        </rect>
-                    </svg>
-                    <div id="Contact">
-                        <span>Contact</span>
-                    </div>
-                </div>
-            </div>
+            </Fragment>
         )
     }
 }
