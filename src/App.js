7c70b012a5c0b51ca82bc79e845cfae9c67b0dc6
Juan E Daza <juandape@unisabana.edu.co>
2021-08-16 19:59:01 -0500

    React import app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import React from 'react'
 import BannerPrincipal from './BannerPrincipal';
 import BannerTwitch from './BannerTwitch';
 import img1 from './img/foto_twitch.png'
-import img2 from './img/Fondo.png'
+import img2 from './img/fondo.png'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div id="LangingPage1Parcial1TI">
         <img id="Fondo" src={img2} alt="FONDO"></img>
         <BannerPrincipal />
-        <img id="foto_twitch" src={img1} alt="FONDO"></img>
+        <img id="foto_twitch" src={img1} alt="ImagenStream"></img>
         <BannerTwitch />
       </div>
     </div>
